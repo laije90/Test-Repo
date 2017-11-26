39152ca37e845c948b5745783bc03c157c73ed82
unknown <Jeffrey.jh.lai@gmail.com>
2017-11-26 15:00:48 -0500

    First Commit

---
+++ Sandwich example.py
@@ -0,0 +1,22 @@
+sandwich_orders = ['Ham','pastrami','cheese','onions','PAstrami','pasTrami','taco','bbq']
+finished_sandwiches = []
+
+print("Tough Luck for the pastrami lovers")
+
+#while 'pastrami' in sandwich_orders:
+#    sandwich_orders.remove('pastrami')
+
+lost_ingredient = 'Pastrami'
+
+# use List > Filter > lambda to circumvent cases sensitivity
+sandwich_orders = list(filter(lambda lost_ingredient: lost_ingredient.lower()!= 'pastrami',sandwich_orders))
+
+
+while sandwich_orders:
+    current_order = sandwich_orders.pop()
+    finished_sandwiches.append(current_order)
+
+print("\nThe folowing Sandwiches have been made:")
+for finished_sandwiches in finished_sandwiches:
+    print(finished_sandwiches.title())
+    
